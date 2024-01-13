7c315615020ae1581367c3a2b97e17ef4729a6d7
uncle-Jonik <Katsimon_Eu@outlook.com>
2024-01-13 14:43:33 +0200

    2/3 task compleate

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,12 +1,21 @@
 import css from './FriendListItem.module.css';
-// import clsx from 'clsx';
+import { LuDiamond } from 'react-icons/lu';
+import clsx from 'clsx';
 
 export const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <div className={css.item}>
       <img className={css.avatar} src={avatar} alt={name} width="48" />
       <p className={css.name}>{name}</p>
-      <p className={css.status}>{isOnline ? 'online' : 'ofline'}</p>
+      <p
+        className={clsx(css.status, {
+          [css.isOnline]: isOnline,
+          [css.isOfline]: !isOnline,
+        })}
+      >
+        <LuDiamond />
+        {isOnline ? 'online' : 'ofline'}
+      </p>
     </div>
   );
 };
